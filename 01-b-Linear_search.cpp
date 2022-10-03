8c7ddcf2b19ad3b4e6ea3179d58263fa82bdfbb0
BHARGAVA ABHIRAM UMMADISHETTY <91135759+abhiram-ummadishetty@users.noreply.github.com>
2022-10-03 20:18:56 +0530

    Create 01-b-Linear_search.cpp

---
+++ 01-b-Linear_search.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+ int data,i,n=5;
+  int ar[5]={1,2,3,4,5};
+  data=3;
+  for(i=0;i<n;i++)
+    {
+      if(data==ar[i])
+      {
+        cout<<"number is at index "<<i<<endl;
+      }
+      
+        }
+  
+  return 0;
+  }
